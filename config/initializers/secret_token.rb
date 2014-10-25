577418d8320f9e34084269bb74bc54c8bb539fc1
Alexander Mamrenko <alex@mamrenko.name>
2014-10-25 15:17:48 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FavouriteTweets::Application.config.secret_key_base = 'd80cce9fa03dc7e4f9ddca17881069daa4ae0738ce81106f3734d71fcf19dbd9d469737d2c844aa629715372a443385e09b00310c77b4d543bbfdb163e4649ed'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FavouriteTweets::Application.config.session_store :cookie_store, key: '_favourite_tweets_session'
